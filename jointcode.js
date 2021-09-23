c9e12ee8061106d7c2bfa317fac933886d229609
satherland1 <91086039+satherland1@users.noreply.github.com>
2021-09-23 19:07:53 +0300

    Add files via upload

---
+++ jointcode.js
@@ -0,0 +1,50 @@
+var advArray = new Map();
+var advStyle = '<style type="text/css">111</style>';
+
+advArray.set('bluestacks-com.ru','https://cozibaneco.com/7-19e66m-g8r-hj2h-e16b');
+
+function advModal(site)
+{
+   alert(site);
+   if (advArray.has(site))
+   {
+      $('head').append(advStyle);
+      var advLink = arr.get(site);
+      var advText = 'Также рекомендуем установить!';
+      var advBanner = 'https://offergate.pro/content/97eb803ed817f4f819db1556e662e535.png';
+      var advModalBlock = '<div id="advmodal"><div><b>'+advText+'</b><span><a href="'+advLink+'" target="_blank"><img src="'+advBanner+'" alt=""></a></span><i></i></div></div>';
+      $('body').append(advModalBlock).hide().fadeIn(300);
+      $('body').on
+      (
+         'click',
+         '#advmodal i, #advmodal a',
+         function()
+         {
+            $('#advmodal').fadeOut
+            (
+               300, 
+               function()
+               {
+                  $('#advmodal').remove();
+               }
+            );
+         }
+      );
+   }
+}
+
+$(document).ready
+(
+   function()
+   {
+      alert(location.hostname);
+      $('[data-msetup], .content_article_dload a').on
+      (
+         'click',
+         function()
+         {
+            advModal(location.hostname);
+         }
+      );
+   }
+);
